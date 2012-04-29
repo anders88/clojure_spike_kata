9361bab42d22dc0a2503e7b9506c17a703376983
Anders Karlsen <karlsenanders@hotmail.com>
2012-04-29 17:07:02 +0200

    Added frontpage

--- project.clj
+++ project.clj
@@ -3,4 +3,5 @@
   :dependencies [[org.clojure/clojure "1.3.0"]
                  [org.clojure/tools.logging "0.2.3"]
                  [noir "1.3.0-alpha10"]
-                 [com.novemberain/monger "1.0.0-beta2"]])
+                 [com.novemberain/monger "1.0.0-beta2"]]
+:main ^{:skip-aot true} clojure_spike_kata.server)
--- src/clojure_spike_kata/server.clj
+++ src/clojure_spike_kata/server.clj
@@ -6,7 +6,7 @@
 (defn -main [& m]
   (let [mode (keyword (or (first m) :dev))
         port (Integer. (get (System/getenv) "PORT" "1337"))
-        mongo-uri (get (System/getenv) "MONGOLAB_URI" "mongodb://127.0.0.1/todo15m")]
+        mongo-uri (get (System/getenv) "MONGOLAB_URI" "mongodb://127.0.0.1/clojure_spike_kata")]
     (monger-connect! mongo-uri)
     (server/start port {:mode mode
                         :ns 'clojure_spike_kata})))

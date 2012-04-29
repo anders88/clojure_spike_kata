94871d85868d6ae56d451011b5738fc6d3138266
Anders Karlsen <karlsenanders@hotmail.com>
2012-04-29 16:52:28 +0200

    Setup. Copied skeleton

---
+++ project.clj
@@ -0,0 +1,6 @@
+(defproject clojure_spike_kata "1.0.0-SNAPSHOT"
+  :description "FIXME: write description"
+  :dependencies [[org.clojure/clojure "1.3.0"]
+                 [org.clojure/tools.logging "0.2.3"]
+                 [noir "1.3.0-alpha10"]
+                 [com.novemberain/monger "1.0.0-beta2"]])
---
+++ src/clojure_spike_kata/server.clj
@@ -0,0 +1,12 @@
+(ns clojure_spike_kata.server
+  (:use [clojure_spike_kata.helpers])
+  (:require [noir.server :as server]
+            [clojure_spike_kata.views]))
+
+(defn -main [& m]
+  (let [mode (keyword (or (first m) :dev))
+        port (Integer. (get (System/getenv) "PORT" "1337"))
+        mongo-uri (get (System/getenv) "MONGOLAB_URI" "mongodb://127.0.0.1/todo15m")]
+    (monger-connect! mongo-uri)
+    (server/start port {:mode mode
+                        :ns 'clojure_spike_kata})))
